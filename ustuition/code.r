c6ec2072b65fede333658ae8f9a7358ca5dd29a7
Thomas Mock <j.thomasmock@gmail.com>
2018-04-02 15:51:49 -0500

    Update code.r

--- ustuition/code.r
+++ ustuition/code.r
@@ -3,11 +3,12 @@ library(readxl)
 
 cost_df <- read_xlsx("us_avg_tuition.xlsx")
 
-cost_df %>% 
+example_plot <- cost_df %>% 
     gather(year, tuition, `2004-05`:`2015-16`) %>% 
     filter(year == "2015-16") %>% 
     ggplot(aes(x = fct_reorder(State, tuition), y = tuition)) +
-        geom_bar(stat = "identity") + 
-        coord_flip() +
+    geom_bar(stat = "identity") + 
+    coord_flip() +
     labs(x = "State", y = "Tuition ($)")
 
+ggsave("example_plot.png", example_plot, height = 12, width = 6, units = "in", dpi = 600)
